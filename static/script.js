1560abd2daf08c41a95cbeab767a640c2f171de9
prithviii-12 <prithvirajdomadia@gmail.com>
2026-06-06 22:30:27 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,21 +8,17 @@ function searchProduct() {
             let resultsDiv = document.getElementById("results");
             resultsDiv.innerHTML = "";
 
-            if (data.length === 0) {
-                resultsDiv.innerHTML = "<p>No products found</p>";
-                return;
-            }
-
             data.forEach((item, index) => {
-                let highlight = index === 0 ? "🏆 Best Deal" : "";
+                let badge = index === 0 ? "🔥 Best Price" : "";
 
                 resultsDiv.innerHTML += `
-                    <div class="result-card">
+                    <div class="card">
+                        <img src="${item.image}">
                         <h3>${item.title}</h3>
-                        <p><b>${item.site}</b></p>
-                        <p>₹ ${item.price}</p>
-                        <p style="color: green;">${highlight}</p>
-                        <img src="${item.link}" width="120">
+                        <p>${item.site}</p>
+                        <p class="price">₹ ${item.price}</p>
+                        <p class="best">${badge}</p>
+                        <a href="${item.link}" target="_blank">View</a>
                     </div>
                 `;
             });
